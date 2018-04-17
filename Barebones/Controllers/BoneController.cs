0ca6586899acad3d570552dc5fa0d94c17b33924
Kyle Hammond <Kyle.Hammond@concordiaplans.org>
2018-04-16 19:53:03 -0500

    Basic controller, layout, and view

---
+++ Barebones/Controllers/BoneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barebones.Controllers
+{
+    public class BoneController : Controller
+    {
+        // GET: Bone
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
